27aa620bbe623e3c86cf74020db8f40962fb8971
samxara <35997471+samxara@users.noreply.github.com>
2019-10-22 23:49:17 +0400

    Add files via upload

---
+++ firstinterface.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public interface FirstInterface {
+    void NumbersBetweenAandB(int a, int b);
+    void SeparateNumbers(int a);
+    void EasySeparateNumbers(int b);
+    int NumberThatIsOftenRepeated();
+
+}
